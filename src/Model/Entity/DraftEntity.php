988065c8729bee292c7bd51f39d1f2c0d417feb8
Robin van der Vleuten <robin@webstronauts.co>
2017-01-03 14:31:29 +0100

    Added StyleCI config + applied all mentioned fixes.

--- src/Model/Entity/DraftEntity.php
+++ src/Model/Entity/DraftEntity.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Draft.php library.
  *
- * (c) Webstronauts <contact@webstronauts.co>
+ * (c) The Webstronauts <contact@webstronauts.co>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -63,7 +63,7 @@ class DraftEntity
 
     public static function create($type, $mutability, $data = null)
     {
-        return self::add(new DraftEntity($type, $mutability, $data));
+        return self::add(new self($type, $mutability, $data));
     }
 
     public static function add(DraftEntity $instance)
